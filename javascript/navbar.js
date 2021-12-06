bf7149b9ca0205e0cde49df6285277df9392fff3
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2021-12-06 20:52:04 +0100

    Navbar med responsive widths

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,16 +1,22 @@
-// Function der åbner nav baren, og tilknyttet til burgermenuen//
+// Function der åbner nav baren, og tilknyttet til burgermenuen// 
 function openNav() {
+  //Hvis skærm størrelsen er mindst 1080pxs bliver width på sidebaren 30% ellers 70%
+  if (window.matchMedia("(min-width: 1080px)").matches){
+    document.getElementById("mySidenav").style.width = "30%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+else {
     document.getElementById("mySidenav").style.width = "70%";
     document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
   }
-  
+}
   // Function der lukker navbaren med at skifte width til 0, tilknyttet til X-et //
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.body.style.backgroundColor = "white";
   }
 
-  //Med hjælp fra W3Schools, functionen der giver et dropdown menu til atleter, når sidebaren er active.
+  //Med hjælp fra W3Schools, functionen der giver et dropdown menu til atleter, når der bliver trykket på atleter.
   var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 for (i = 0; i < dropdown.length; i++) {
