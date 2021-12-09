9db41bfa516b58ea44635e9837e84e0db439b533
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2021-12-09 11:12:29 +0100

    Lightbox gallery

--- javascript/atlet.js
+++ javascript/atlet.js
@@ -26,3 +26,44 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " activedot";
 }
+
+//Lightbox Modal Open
+function openModal() {
+  document.getElementById("modal").style.display = "block";
+}
+
+//Luk Lightbox Modal
+function closeModal() {
+  document.getElementById("modal").style.display = "none";
+}
+
+var slideLightboxIndex = 1;
+showLightboxSlides(slideLightboxIndex);
+
+// Next/previous controls
+function plusLightboxSlides(n) {
+  showLightboxSlides(slideLightboxIndex += n);
+}
+
+// Thumbnail image controls
+function currentLightboxSlide(n) {
+  showLightboxSlides(slideLightboxIndex = n);
+}
+
+function showLightboxSlides(n) {
+  var i;
+  var lightboxslides = document.getElementsByClassName("lightboxSlides");
+  var lightboxdots = document.getElementsByClassName("lightboxdot");
+  var captionText = document.getElementById("caption");
+  if (n > lightboxslides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = lightboxslides.length}
+  for (i = 0; i < lightboxslides.length; i++) {
+    lightboxslides[i].style.display = "none";
+  }
+  for (i = 0; i < lightboxdots.length; i++) {
+    lightboxdots[i].className = lightboxdots[i].className.replace(" active", "");
+  }
+  lightboxslides[slideIndex-1].style.display = "block";
+  lightboxdots[slideIndex-1].className += " active";
+  captionText.innerHTML = lightboxdots[slideIndex-1].alt;
+}
